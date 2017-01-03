863684bed3588c7f0009ac356dd0be499f1e323a
Peihong Guo <peihongguo@gmail.com>
2017-01-03 16:46:39 -0600

    works with CLion now

---
+++ intel-compiler.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(intel_compiler_path "/opt/intel/bin/intel64/")
+
+set(CMAKE_C_COMPILER "${intel_compiler_path}/icc")
+set(CMAKE_CXX_COMPILER "${intel_compiler_path}/icpc")
